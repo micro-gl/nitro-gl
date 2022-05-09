f96f70cae25061d5c29b7c0443b363b00ba1d48f
Tomer Riko Shalev <tomer.shalev@gmail.com>
2022-05-09 22:45:08 +0300

    finish

---
+++ examples/ex_sampler_gradient_angular.cpp
@@ -0,0 +1,53 @@
+#define GL_SILENCE_DEPRECATION
+#define SUPPORTS_VAO
+#define MICROGL_USE_STD_MATH
+#define NITROGL_ENABLE_THROW
+
+#include "src/example.h"
+#include "src/Resources.h"
+#include <nitrogl/samplers/test_sampler.h>
+#include <nitrogl/samplers/mix_sampler.h>
+#include <nitrogl/samplers/texture_sampler.h>
+#include <nitrogl/samplers/circle_sampler.h>
+#include <nitrogl/samplers/angular_gradient.h>
+#include <nitrogl/canvas.h>
+
+using namespace nitrogl;
+
+int main() {
+
+    auto on_init = [](SDL_Window *, void *) {
+        auto tex = gl_texture(500,500);
+        glCheckError();
+        canvas canva(500,500);
+        auto tex_sampler_1 = texture_sampler<true>(Resources::loadTexture("assets/images/test.png", true));
+        auto tex_sampler_2 = texture_sampler<true>(Resources::loadTexture("assets/images/test.png", false));
+        auto tex_sampler_3 = texture_sampler<false>(Resources::loadTexture("assets/images/uv_256.png", true));
+
+        angular_gradient gradient;
+
+        gradient.addStop(0.0f, {1,0,0, 1});
+        gradient.addStop(0.50f, {0,1,0, 1});
+        gradient.addStop(1.f, {0,0,1, 1});
+
+        auto render = [&]() {
+            static float t= 0;
+            t+=0.05;
+
+            // some cool animations
+//            gradient.setNewRadial({0.5f, 0.5f}, (1.25f+std::sin(t/100.0f))/2.0f);
+//            gradient.setNewRadial({std::sin(t/1.0f), 0.5f}, 0.5f);
+//            gradient.updateStop(3, 1.0f, {1.0, 0.0, 1.0, (std::sin(t/10.0f)+1.0f)/2.0f});
+
+            canva.clear(1.0, 1.0, 1.0, 1.0);
+            canva.drawRect(tex_sampler_3, 0, 0, 250, 250);//, mat3f::rotation(t));
+            canva.drawRect(gradient, 0, 0, 250, 250, 1.0);
+            glCheckError();
+        };
+
+        example_run(canva, render);
+    };
+
+    example_init(on_init);
+}
+
