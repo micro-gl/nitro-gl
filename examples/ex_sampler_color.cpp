260ac9ac2897009effc2e3f9e94531703506500a
Tomer Riko Shalev <tomer.shalev@gmail.com>
2022-05-20 17:56:44 +0300

    finish and some bug fixes

---
+++ examples/ex_sampler_color.cpp
@@ -0,0 +1,34 @@
+#define GL_SILENCE_DEPRECATION
+#define SUPPORTS_VAO
+#define MICROGL_USE_STD_MATH
+#define NITROGL_ENABLE_THROW
+
+#include "src/example.h"
+#include "src/Resources.h"
+#include <nitrogl/samplers/color_sampler.h>
+#include <nitrogl/canvas.h>
+
+using namespace nitrogl;
+
+int main() {
+
+    auto on_init = [](SDL_Window *, void *) {
+        auto tex = gl_texture(500,500);
+        glCheckError();
+        canvas canva(500,500);
+        color_sampler sampler_color(1.0,0.0,1.0,1.0);
+
+        auto render = [&]() {
+            static float t= 0;
+            t+=0.05;
+            canva.clear(1.0, 1.0, 1.0, 1.0);
+            canva.drawRect(sampler_color, 0, 0, 250, 250);
+            glCheckError();
+        };
+
+        example_run(canva, render);
+    };
+
+    example_init(on_init);
+}
+
---
+++ include/nitrogl/samplers/block_sampler.h
@@ -0,0 +1,66 @@
+/*========================================================================================
+ Copyright (2021), Tomer Shalev (tomer.shalev@gmail.com, https://github.com/HendrixString).
+ All Rights Reserved.
+ License is a custom open source semi-permissive license with the following guidelines:
+ 1. unless otherwise stated, derivative work and usage of this file is permitted and
+    should be credited to the project and the author of this project.
+ 2. Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+========================================================================================*/
+#pragma once
+
+#include <nitrogl/samplers/sampler.h>
+#include <nitrogl/traits.h>
+#include <nitrogl/color.h>
+
+namespace nitrogl {
+
+    /**
+     * A Block Sampler. Use blocks count that divides the image size without
+     * remainder
+     */
+    struct block_sampler : public multi_sampler<1> {
+        using base = multi_sampler<1>;
+        const char * name() const override { return "color_sampler"; }
+        const char * uniforms() const override {
+            return R"(
+{
+    float blocks;
+}
+)";
+        }
+
+        const char * other_functions() const override {
+            return R"(
+vec4 other_function(float t) {
+    return vec4(t);
+}
+)";
+        }
+
+        const char * main() const override {
+            return R"(
+(vec3 uv) {
+    uv.x = float(int(uv.x*data.blocks))/data.blocks;
+    uv.y = float(int(uv.y*data.blocks))/data.blocks;
+    return sampler_00(uv);
+}
+)";
+        }
+
+        void on_cache_uniforms_locations(GLuint program) override {
+        }
+
+        void on_upload_uniforms_request(GLuint program) override {
+            GLint loc = get_uniform_location(program, "blocks");
+            glUniform1f(loc, float(blocks));
+        }
+
+        unsigned int blocks;
+
+    public:
+        explicit block_sampler(sampler_t * sampler, unsigned int blocks=5) :
+            blocks(blocks), base(sampler) {}
+    };
+}
